572ac76c7126a5bf8572bf11362bbd607df54fef
Thibault Duretz <tduretz@pulse-207.unil.ch>
2019-05-27 11:37:02 +0200

    emptied User.c

--- SOURCE/User.c
+++ SOURCE/User.c
@@ -41,10 +41,6 @@ void SetBCs_user( grid *mesh, params *model, scale scaling, markers* particles, 
     double *X, *Z, *XC, *ZC;
     int   NX, NZ, NCX, NCZ;
     
-    int StressBC_W=0, StressBC_E=0;
-    if ( model->user0 == 1 ) StressBC_E = 1;
-    double TN = 273.15/scaling.T, TS = 1330/scaling.T;
-    double TW = 273.15/scaling.T, TE = 1330/scaling.T;
     
     NX  = mesh->Nx;
     NZ  = mesh->Nz;
@@ -89,48 +85,6 @@ void SetBCs_user( grid *mesh, params *model, scale scaling, markers* particles, 
             
             if ( mesh->BCu.type[c] != 30 ) {
                 
-                // Internal points:  -1
-                mesh->BCu.type[c] = -1;
-                mesh->BCu.val[c]  =  0;
-                
-                // Matching BC nodes WEST
-                if (k==0 && l>0 && l<mesh->Nz ) { //
-                    
-                    if ( StressBC_W==1 ) {
-                        mesh->BCu.type[c] = 2;
-                        c1 = k + (l-1)*(mesh->Nx-1) ;
-                        mesh->BCu.val[c]  = -mesh->p_lith[c1];
-                    }
-                    else {
-                        mesh->BCu.type[c] = 0;
-                        mesh->BCu.val[c]  = 0.0;
-                    }
-                }
-                
-                // Matching BC nodes EAST
-                if (k==mesh->Nx-1 && l>0 && l<mesh->Nz) { //
-                    if ( StressBC_E==1 ) {
-                        mesh->BCu.type[c] = 2;
-                        c1 = k + (l-1)*(mesh->Nx-1) -1;
-                        mesh->BCu.val[c]  = -mesh->p_lith[c1];
-                    }
-                    else {
-                        mesh->BCu.type[c] = 0;
-                        mesh->BCu.val[c]  = 0.0;
-                    }
-                }
-                
-                
-                // Free slip
-                if (l==0  ) {
-                    mesh->BCu.type[c] = 13;
-                    mesh->BCu.val[c]  =  0;
-                }
-                
-                if ( l==mesh->Nz ) {
-                    mesh->BCu.type[c] = 13;
-                    mesh->BCu.val[c]  =  0;
-                }
             }
             
         }
@@ -160,47 +114,8 @@ void SetBCs_user( grid *mesh, params *model, scale scaling, markers* particles, 
                 mesh->BCv.type[c] = -1;
                 mesh->BCv.val[c]  =  0;
 
-                // Matching BC nodes SOUTH
-                if (l==0  && k>0 && k<mesh->Nx) {
-                    mesh->BCv.type[c] = 0;
-                    mesh->BCv.val[c]  = 0.0;
-                }
+                
 
-                // Matching BC nodes NORTH
-                if (l==mesh->Nz-1  && k>0 && k<mesh->Nx) {
-                    mesh->BCv.type[c] = 0;
-                    mesh->BCv.val[c]  = 0.0;
-                }
-
-                // Non-matching boundary points
-                if ( (k==0) ) {
-                    if ( StressBC_W==1 ) {
-                        mesh->BCv.type[c] =   11;
-                        mesh->BCv.val[c]  =   0;
-                    }
-                    else {
-                        mesh->BCv.type[c] =   13;
-                        mesh->BCv.val[c]  =   0;
-                    }
-                }
-
-                // Non-matching boundary points
-                if ( (k==mesh->Nx) ) {
-                    if ( StressBC_E==1 ) {
-                        mesh->BCv.type[c] =   11;
-                        mesh->BCv.val[c]  =   0;
-                    }
-                    else{
-                        mesh->BCv.type[c] =   13;
-                        mesh->BCv.val[c]  =   0;
-                    }
-                }
-
-                //                    // Normal stress applied to the free surface
-                //                    if (l<mesh->Nz-1 && k>0 && k<mesh->Nx) {
-                //                        c1 = k + (l)*(mesh->Nx-1)-1;
-                //                        if (mesh->BCp.type[0][c1]==31) mesh->BCv.val[c]  =  -0*mesh->p_lith[c1-(mesh->Nx-1)];
-                //                    }
             }
         }
     }
@@ -243,42 +158,6 @@ void SetBCs_user( grid *mesh, params *model, scale scaling, markers* particles, 
             c = k + l*(NCX);
             
             if ( mesh->BCt.type[c] != 30 ) {
-                
-                // WEST
-                if ( k==0 ) {
-                    mesh->BCt.type[c] = 0;
-                    mesh->BCt.typW[l] = 0;
-                    mesh->BCt.valW[l] = TW;
-                }
-                
-                // EAST
-                if ( k==NCX-1 ) {
-                    mesh->BCt.type[c] = 0;
-                    mesh->BCt.typE[l] = 0;
-                    mesh->BCt.valE[l] = TE;
-                }
-                
-                // SOUTH
-                if ( l==0 ) {
-                    mesh->BCt.type[c] = 0;
-                    mesh->BCt.typS[k] = 1;
-                    mesh->BCt.valS[k] = TS;
-                }
-                
-                // NORTH
-                if ( l==NCZ-1 ) {
-                    mesh->BCt.type[c] = 0;
-                    mesh->BCt.typN[k] = 1;
-                    mesh->BCt.valN[k] = TN;
-                }
-                
-                // FREE SURFACE
-                else {
-                    if ((mesh->BCt.type[c] == -1 || mesh->BCt.type[c] == 1 || mesh->BCt.type[c] == 0) && mesh->BCt.type[c+NCX] == 30) {
-                        mesh->BCt.type[c] = 1;
-                        mesh->BCt.val[c]  = TN;
-                    }
-                }
                 
                 
             }
