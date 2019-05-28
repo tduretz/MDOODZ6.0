a905a98e7d5eac6769de5f85baf39e3be8b22fe0
Arthur Bauville <a.bauville@gmail.com>
2019-05-28 18:55:44 +0900

    Revert "git rm --cached"
    This reverts commit 1e1f6b805f7828588064405e3d45130d388c65ba.

---
+++ SOURCE/User.c
@@ -0,0 +1,185 @@
+// =========================================================================
+// MDOODZ - Visco-Elasto-Plastic Thermo-Mechanical solver
+//
+// Copyright (C) 2018  MDOODZ Developper team
+//
+// This file is part of MDOODZ.
+//
+// MDOODZ is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// MDOODZ is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with MDOODZ.  If not, see <http://www.gnu.org/licenses/>.
+// =========================================================================
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "math.h"
+#include "header_MDOODZ.h"
+#include "stdbool.h"
+
+#ifdef _VG_
+#define printf(...) printf("")
+#endif
+
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+/*------------------------------------------------------ M-Doodz -----------------------------------------------------*/
+/*--------------------------------------------------------------------------------------------------------------------*/
+// Set physical properties on the grid and boundary conditions
+void SetBCs_user( grid *mesh, params *model, scale scaling, markers* particles, mat_prop *materials ) {
+    
+    int   k, l, c, c1;
+    double *X, *Z, *XC, *ZC;
+    int   NX, NZ, NCX, NCZ;
+
+    NX  = mesh->Nx;
+    NZ  = mesh->Nz;
+    NCX = NX-1;
+    NCZ = NZ-1;
+
+
+
+
+
+
+    
+    
+    X  = malloc (NX*sizeof(double));
+    Z  = malloc (NZ*sizeof(double));
+    XC = malloc (NCX*sizeof(double));
+    ZC = malloc (NCZ*sizeof(double));
+    
+    for (k=0; k<NX; k++) {
+        X[k] = mesh->xg_coord[k];
+    }
+    for (k=0; k<NCX; k++) {
+        XC[k] = mesh->xc_coord[k];
+    }
+    for (l=0; l<NZ; l++) {
+        Z[l] = mesh->zg_coord[l];
+    }
+    for (l=0; l<NCZ; l++) {
+        ZC[l] = mesh->zc_coord[l];
+    }
+    
+    /* --------------------------------------------------------------------------------------------------------*/
+    /* Set the BCs for Vx on all grid levels                                                                   */
+    /* Type  0: Dirichlet point that matches the physical boundary (Vx: left/right, Vz: bottom/top)            */
+    /* Type 11: Dirichlet point that do not match the physical boundary (Vx: bottom/top, Vz: left/right)       */
+    /* Type  2: Neumann point that do not match the physical boundary (Vx: bottom/top, Vz: left/right)         */
+    /* Type 13: Neumann point that matches the physical boundary (Vx: bottom/top, Vz: left/right)              */
+    /* Type -2: periodic in the x direction (matches the physical boundary)                                    */
+    /* Type -1: not a BC point (tag for inner points)                                                          */
+    /* Type 30: not calculated (part of the "air")                                                             */
+    /* --------------------------------------------------------------------------------------------------------*/
+    
+    //--------------------------------------------------------------------------------
+    
+    for (l=0; l<mesh->Nz+1; l++) {
+        for (k=0; k<mesh->Nx; k++) {
+            
+            c = k + l*(mesh->Nx);
+            
+            if ( mesh->BCu.type[c] != 30 ) {
+                
+            }
+            
+        }
+    }
+    
+    
+    /* --------------------------------------------------------------------------------------------------------*/
+    /* Set the BCs for Vz on all grid levels                                                                   */
+    /* Type  0: Dirichlet point that matches the physical boundary (Vx: left/right, Vz: bottom/top)            */
+    /* Type 11: Dirichlet point that do not match the physical boundary (Vx: bottom/top, Vz: left/right)       */
+    /* Type  2: Neumann point that do not match the physical boundary (Vx: bottom/top, Vz: left/right)         */
+    /* Type 13: Neumann point that matches the physical boundary (Vx: bottom/top, Vz: left/right)              */
+    /* Type -2: periodic in the x direction (does not match the physical boundary)                             */
+    /* Type-10: useless point (set to zero)                                                                    */
+    /* Type -1: not a BC point (tag for inner points)                                                          */
+    /* Type 30: not calculated (part of the "air")                                                             */
+    /* --------------------------------------------------------------------------------------------------------*/
+
+    for (l=0; l<mesh->Nz; l++) {
+        for (k=0; k<mesh->Nx+1; k++) {
+            
+            c  = k + l*(mesh->Nx+1);
+            
+            if ( mesh->BCv.type[c] != 30 ) {
+
+                // Internal points:  -1
+                mesh->BCv.type[c] = -1;
+                mesh->BCv.val[c]  =  0;
+
+                
+
+            }
+        }
+    }
+
+    
+    /* --------------------------------------------------------------------------------------------------------*/
+    /* Set the BCs for P on all grid levels                                                                    */
+    /* Type  0: Dirichlet within the grid                                                                      */
+    /* Type -1: not a BC point (tag for inner points)                                                          */
+    /* Type 30: not calculated (part of the "air")                                                             */
+    /* Type 31: surface pressure (Dirichlet)                                                                   */
+    /* --------------------------------------------------------------------------------------------------------*/
+    
+    for (l=0; l<NCZ; l++) {
+        for (k=0; k<NCX; k++) {
+            
+            c  = k + l*(NCX);
+            
+            if (mesh->BCt.type[c] != 30) {
+                
+                // Internal points:  -1
+                mesh->BCp.type[c] = -1;
+                mesh->BCp.val[c]  =  0;
+            }
+        }
+    }
+    
+    /* -------------------------------------------------------------------------------------------------------*/
+    /* Set the BCs for T on all grid levels                                                                   */
+    /* Type  1: Dirichlet point that do not match the physical boundary (Vx: bottom/top, Vz: left/right)      */
+    /* Type  0: Neumann point that matches the physical boundary (Vx: bottom/top, Vz: left/right)             */
+    /* Type -2: periodic in the x direction (matches the physical boundary)                                   */
+    /* Type -1: not a BC point (tag for inner points)                                                         */
+    /* Type 30: not calculated (part of the "air")                                                            */
+    /* -------------------------------------------------------------------------------------------------------*/
+    
+    for (l=0; l<mesh->Nz-1; l++) {
+        for (k=0; k<mesh->Nx-1; k++) {
+            
+            c = k + l*(NCX);
+            
+            if ( mesh->BCt.type[c] != 30 ) {
+                
+                
+            }
+            
+        }
+    }
+    
+    free(X);
+    free(Z);
+    free(XC);
+    free(ZC);
+    printf("Velocity and pressure were initialised\n");
+    printf("Boundary conditions were set up\n");
+    
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+/*------------------------------------------------------ M-Doodz -----------------------------------------------------*/
+/*--------------------------------------------------------------------------------------------------------------------*/
