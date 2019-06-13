3a7f240ae14bd68249b179dfed3c58c560aac412
abauville <a.bauville@gmail.com>
2019-06-13 11:29:04 +0900

    added Benchmark folder

---
+++ Benchmarks/run_tests.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 12 15:33:39 2019
+@author: abauville
+
+The purpose of this file is to run a series of tests to ensure that the code
+is behaving the way it should
+
+This test series should be run and the results should be sent to the admin 
+when submitting a pull request
+
+"""
+
+# For list of tests
+
+# Run ../Setups/subBenchCase3
+# Input files are created in the Input folder
+
+# Run mdoodz - Output files should be written in the Output folder
+
+# Run ../PostProcessing/subBenchCase3
+
+# Write test summary to some file
+
+# clean Input and Output
+# do next Benchmark
